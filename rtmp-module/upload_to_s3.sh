7f9ecc180709534ca79f2733028ac379cbde9d51
Zackry Langford <104527780+zackrylangford@users.noreply.github.com>
2023-11-21 14:13:57 -0500

    Update file name with timestamp before uploading to S3

--- rtmp-module/upload_to_s3.sh
+++ rtmp-module/upload_to_s3.sh
@@ -6,6 +6,12 @@ BUCKET_NAME="YOUR_BUCKET_NAME_HERE"
 # Find the latest file in the directory
 LATEST_FILE=$(ls -Art $UPLOAD_DIR | tail -n 1)
 if [[ -f "$UPLOAD_DIR/$LATEST_FILE" ]]; then
-    # Upload to S3
-    aws s3 cp "$UPLOAD_DIR/$LATEST_FILE" "s3://$BUCKET_NAME/$LATEST_FILE" && rm -f "$UPLOAD_DIR/$LATEST_FILE"
+    # Generate a timestamp
+    TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
+
+    # Generate new file name with timestamp
+    NEW_FILE_NAME="${TIMESTAMP}-${LATEST_FILE}"
+
+    # Upload to S3 with new file name
+    aws s3 cp "$UPLOAD_DIR/$LATEST_FILE" "s3://$BUCKET_NAME/$NEW_FILE_NAME" && rm -f "$UPLOAD_DIR/$LATEST_FILE"
 fi
