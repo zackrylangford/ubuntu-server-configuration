cd1b80eae655788edb7f821c47ad99d2cbcc9d69
Zackry Langford <104527780+zackrylangford@users.noreply.github.com>
2023-11-22 19:43:07 -0500

    Updated script to accomodate more than one date
    This provides a clean way for the script to upload

--- rtmp-module/upload_to_s3.sh
+++ rtmp-module/upload_to_s3.sh
@@ -1,17 +1,31 @@
-#!/bin/bash
-
 UPLOAD_DIR="/var/www/livestream-archives"
 BUCKET_NAME="YOUR_BUCKET_NAME_HERE"
 
 # Find the latest file in the directory
 LATEST_FILE=$(ls -Art $UPLOAD_DIR | tail -n 1)
 if [[ -f "$UPLOAD_DIR/$LATEST_FILE" ]]; then
-    # Generate a timestamp
-    TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
+    # Generate a date string in the MM-DD-YYYY format
+    DATE_STR=$(date +"%m-%d-%Y")
 
-    # Generate new file name with timestamp
-    NEW_FILE_NAME="${TIMESTAMP}-${LATEST_FILE}"
+    # Create the base new file name
+    BASE_NEW_FILE_NAME="sunday-morning-worship-${DATE_STR}"
+
+    # Initialize the full new file name
+    NEW_FILE_NAME="${BASE_NEW_FILE_NAME}.flv"
+
+    # Initialize a counter
+    COUNTER=1
+
+    # Check if the file already exists in S3
+    while aws s3 ls "s3://$BUCKET_NAME/$NEW_FILE_NAME" &> /dev/null; do
+        # Append a counter to the file name if it already exists
+        NEW_FILE_NAME="${BASE_NEW_FILE_NAME}-${COUNTER}.flv"
+        let COUNTER=COUNTER+1
+    done
+
+    # Rename the latest file to the new name
+    mv "$UPLOAD_DIR/$LATEST_FILE" "$UPLOAD_DIR/$NEW_FILE_NAME"
 
     # Upload to S3 with new file name
-    aws s3 cp "$UPLOAD_DIR/$LATEST_FILE" "s3://$BUCKET_NAME/$NEW_FILE_NAME" && rm -f "$UPLOAD_DIR/$LATEST_FILE"
+    aws s3 cp "$UPLOAD_DIR/$NEW_FILE_NAME" "s3://$BUCKET_NAME/$NEW_FILE_NAME" && rm -f "$UPLOAD_DIR/$NEW_FILE_NAME"
 fi
